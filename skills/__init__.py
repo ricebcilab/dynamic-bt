39aa6570de08aeb45db4e96b2aacadb51f14704e
Siyuan <st121@rice.edu>
2026-07-10 19:04:35 -0500

    New recovery skill for pi-0.5 finetuning

--- skills/__init__.py
+++ skills/__init__.py
@@ -1,6 +1,7 @@
 from .base_skill import BaseSkill, SKILL_REGISTRY
 from .approach_and_align import ApproachAndAlign
 from .descend_and_grasp import DescendAndGrasp
+from .descend_and_grasp_recovery import DescendAndGraspRecovery
 from .retract_object import RetractObject
 from .place_and_release import PlaceAndRelease
 from .retreat import Retreat

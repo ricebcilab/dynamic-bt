0b42d6ecd16000f4c0ef5a57811439ab81719346
Siyuan <st121@rice.edu>
2026-07-30 20:13:59 -0500

    Remove stale recovery skill

--- skills/__init__.py
+++ skills/__init__.py
@@ -2,7 +2,6 @@ from .base_skill import BaseSkill, SKILL_REGISTRY
 from .acquire import Acquire
 from .approach_and_align import ApproachAndAlign
 from .descend_and_grasp import DescendAndGrasp
-from .descend_and_grasp_recovery import DescendAndGraspRecovery
 from .place_and_release import PlaceAndRelease
 from .retract_object import RetractObject
 from .retreat import Retreat
